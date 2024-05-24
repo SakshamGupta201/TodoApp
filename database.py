7648ed9b2a74ec01c83b95e377a4d22744343ac6
Saksham Gupta <guptasaksham495@gmail.com>
2024-05-24 23:47:38 +0530

    added mysql conf

--- database.py
+++ database.py
@@ -4,12 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
